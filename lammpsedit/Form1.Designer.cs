90ff9930005837c29f85c9aede268f50f75518fa
kykougaku <kougaku.yuuki@outlook.jp>
2022-12-26 14:24:19 +0900

    プロジェクト ファイルを追加します。

---
+++ lammpsedit/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace lammpsedit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開く";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(189, 37);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(797, 601);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(36, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "結晶構造データ|*.zinit|すべてのファイル|*.*";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 675);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lattice_editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private TextBox textBox2;
+        private Button button3;
+        private SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ lammpsedit/Form1.cs
@@ -0,0 +1,123 @@
+using System;
+using System.IO;
+
+
+namespace lammpsedit
+{
+    public partial class Form1 : Form
+    {
+        int n = 0;
+        string filename;
+        string savename;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int linenum = 1;
+            int atomdataline = 20;
+
+            textBox2.Text = "";
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                filename = openFileDialog1.FileName;
+
+                foreach (string line in System.IO.File.ReadLines(@filename))
+                {
+                    if (linenum == 2)//2行目で原子数を読み取り
+                    {
+                        string[] words = line.Split(' ');
+                        n = Convert.ToInt32(words[0]);
+                        break;
+                    }
+
+                    linenum++;
+                }
+            }
+
+            //初期化して最初から読み直し(nをもちいて配列宣言をするためにforeachを抜けなければいけない)
+            linenum = 1;
+            double[,] atomdata = new double[n, 5];//原子座標データを格納する二次元配列
+            // l0:Atom_ID l1:Atom_Type l2-4:position x-z
+
+            string[] atomdatas = new string[n+20];
+
+            foreach (string line in System.IO.File.ReadLines(@filename))
+            {
+                if (line == "Atoms  # atomic") atomdataline = linenum + 2;//コメント「toms  # atomic」を見つけたらフラグを立てる
+
+
+                if ( linenum >= atomdataline)//コメント「toms  # atomic」から2行後ろの原子座標データに到達したら
+                {
+                    string[] words = line.Split(' ');
+                    for(int j = 0; j<5; j++)
+                    {
+                        atomdata[linenum - atomdataline, j] = Convert.ToDouble(words[j]);
+                    }
+                }
+                else//原子座標データ前ならbox2に書き込み
+                {
+                    textBox2.Text += line + Environment.NewLine;
+                }
+
+                linenum++;
+            }
+
+            //ここで配列の計算編集処理
+            for(int i = 0; i < n; i++)
+            {
+                if (atomdata[i,3] < -5.0)
+                {
+                    atomdata[i, 1] += 2;
+                }
+            }
+
+            //box2に書き込み
+            for(int i = 0; i < n; i++)
+            {
+                string linew = "";
+                for(int j = 0; j < 5; j++)
+                {
+                    linew += Convert.ToString(atomdata[i,j]) + " ";
+                }
+                textBox2.Text += linew + Environment.NewLine;
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //box2からの保存処理
+            DialogResult dr = saveFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                savename = saveFileDialog1.FileName;
+                StreamWriter sw = new System.IO.StreamWriter(savename, false);
+                sw.Write(textBox2.Text);
+                sw.Close();
+                MessageBox.Show("保存できました", "完了", MessageBoxButtons.OK);//メッセージボックスで完了通知
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
