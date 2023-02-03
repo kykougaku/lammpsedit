02c22c41337ad421cf8f1dea58eddda29a33f30e
kimdon <6489550+kykougaku@users.noreply.github.com>
2023-02-03 23:11:55 +0900

    add lammpstrj file

--- lammpsedit/Form1.Designer.cs
+++ lammpsedit/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -53,9 +54,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(36, 99);
+            this.button1.Location = new System.Drawing.Point(29, 79);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.Size = new System.Drawing.Size(90, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "開く";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,20 +65,22 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(228, 12);
+            this.textBox2.Location = new System.Drawing.Point(182, 10);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.MaxLength = 0;
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox2.Size = new System.Drawing.Size(819, 500);
+            this.textBox2.Size = new System.Drawing.Size(656, 401);
             this.textBox2.TabIndex = 3;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.WordWrap = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(36, 186);
+            this.button3.Location = new System.Drawing.Point(29, 149);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 34);
+            this.button3.Size = new System.Drawing.Size(90, 27);
             this.button3.TabIndex = 4;
             this.button3.Text = "保存";
             this.button3.UseVisualStyleBackColor = true;
@@ -84,15 +88,16 @@
             // 
             // saveFileDialog1
             // 
-            this.saveFileDialog1.Filter = "結晶構造データ|*.zinit|すべてのファイル|*.*";
+            this.saveFileDialog1.Filter = "結晶構造データ|*.zinit|結晶構造アニメーション|*.lammpstrj|すべてのファイル|*.*";
             this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
             // labelx
             // 
             this.labelx.AutoSize = true;
-            this.labelx.Location = new System.Drawing.Point(290, 554);
+            this.labelx.Location = new System.Drawing.Point(232, 443);
+            this.labelx.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelx.Name = "labelx";
-            this.labelx.Size = new System.Drawing.Size(20, 25);
+            this.labelx.Size = new System.Drawing.Size(16, 20);
             this.labelx.TabIndex = 5;
             this.labelx.Text = "x";
             this.labelx.Click += new System.EventHandler(this.label1_Click);
@@ -100,45 +105,50 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(445, 554);
+            this.label2.Location = new System.Drawing.Point(356, 443);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(21, 25);
+            this.label2.Size = new System.Drawing.Size(16, 20);
             this.label2.TabIndex = 6;
             this.label2.Text = "y";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(610, 554);
+            this.label3.Location = new System.Drawing.Point(488, 443);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(20, 25);
+            this.label3.Size = new System.Drawing.Size(16, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "z";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(211, 588);
+            this.label4.Location = new System.Drawing.Point(169, 470);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(42, 25);
+            this.label4.Size = new System.Drawing.Size(34, 20);
             this.label4.TabIndex = 8;
             this.label4.Text = "min";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(211, 628);
+            this.label5.Location = new System.Drawing.Point(169, 502);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(45, 25);
+            this.label5.Size = new System.Drawing.Size(37, 20);
             this.label5.TabIndex = 9;
             this.label5.Text = "max";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(290, 588);
+            this.label6.Location = new System.Drawing.Point(232, 470);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(50, 25);
+            this.label6.Size = new System.Drawing.Size(41, 20);
             this.label6.TabIndex = 10;
             this.label6.Text = "minx";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -146,61 +156,80 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(445, 588);
+            this.label7.Location = new System.Drawing.Point(356, 470);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(51, 25);
+            this.label7.Size = new System.Drawing.Size(41, 20);
             this.label7.TabIndex = 11;
             this.label7.Text = "miny";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(610, 588);
+            this.label8.Location = new System.Drawing.Point(488, 470);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(50, 25);
+            this.label8.Size = new System.Drawing.Size(41, 20);
             this.label8.TabIndex = 12;
             this.label8.Text = "minz";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(290, 628);
+            this.label9.Location = new System.Drawing.Point(232, 502);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(53, 25);
+            this.label9.Size = new System.Drawing.Size(44, 20);
             this.label9.TabIndex = 13;
             this.label9.Text = "maxx";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(445, 628);
+            this.label10.Location = new System.Drawing.Point(356, 502);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(54, 25);
+            this.label10.Size = new System.Drawing.Size(44, 20);
             this.label10.TabIndex = 14;
             this.label10.Text = "maxy";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(610, 628);
+            this.label11.Location = new System.Drawing.Point(488, 502);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(53, 25);
+            this.label11.Size = new System.Drawing.Size(44, 20);
             this.label11.TabIndex = 15;
             this.label11.Text = "maxz";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(26, 400);
+            this.progressBar1.Location = new System.Drawing.Point(21, 320);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(150, 34);
+            this.progressBar1.Size = new System.Drawing.Size(120, 27);
             this.progressBar1.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "zinit",
+            "lammpstrj"});
+            this.comboBox1.Location = new System.Drawing.Point(21, 217);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 17;
             // 
             // Form1
             // 
             this.AllowDrop = true;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1228, 696);
+            this.ClientSize = new System.Drawing.Size(982, 557);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
@@ -216,6 +245,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Lattice_editor";
             this.ResumeLayout(false);
@@ -242,5 +272,6 @@
         private Label label10;
         private Label label11;
         private ProgressBar progressBar1;
+        private ComboBox comboBox1;
     }
 }
--- lammpsedit/Form1.cs
+++ lammpsedit/Form1.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Globalization;
 using System.IO;
-
+using System.Net.NetworkInformation;
+using System.Text;
 
 namespace lammpsedit
 {
@@ -13,106 +15,199 @@ namespace lammpsedit
         public Form1()
         {
             InitializeComponent();
+            comboBox1.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int atomdataline = 20;
-
-            DialogResult dr = openFileDialog1.ShowDialog();
-            if (dr == System.Windows.Forms.DialogResult.OK)
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////zinit
+            if (comboBox1.SelectedItem == "zinit")
             {
-                textBox2.Text = ""; //textboxをリセット
+                int atomdataline = 20;
 
-                filename = openFileDialog1.FileName;
-                string[] readlines = System.IO.File.ReadAllLines(@filename);//ファイル内容を取得
-
-                //2行目で原子数を読み取り
-                string[] words = readlines[1].Split(' ');
-                n = Convert.ToInt32(words[0]);
-
-
-                //"Atoms  # atomic")コメントを探索 → indexから原子座標データの行番号を取得
-                for (int i = 0; i<readlines.Length; i++)
+                DialogResult dr = openFileDialog1.ShowDialog();
+                if (dr == System.Windows.Forms.DialogResult.OK)
                 {
-                    if (readlines[i] == "Atoms  # atomic")
-                    {
-                        atomdataline = i + 2;
-                        break;
-                    }
-                }
-                //一度抜ける
+                    textBox2.Text = ""; //textboxをリセット
 
-                double[,] atomdata = new double[n, 5];//原子座標データを格納する二次元配列
-                // l0:Atom_ID l1:Atom_Type l2-4:position x-z
+                    filename = openFileDialog1.FileName;
+                    string[] readlines = System.IO.File.ReadAllLines(@filename);//ファイル内容を取得
 
-                for (int i = 0; i < readlines.Length; i++)
-                {
-                    if (atomdataline <= i)//コメント「toms  # atomic」から2行後ろの原子座標データに到達したら
+                    //2行目で原子数を読み取り
+                    string[] words = readlines[1].Split(' ');
+                    n = Convert.ToInt32(words[0]);
+
+
+                    //"Atoms  # atomic")コメントを探索 → indexから原子座標データの行番号を取得
+                    for (int i = 0; i < readlines.Length; i++)
                     {
-                        string[] wordss = readlines[i].Split(' ');
-                        for (int j = 0; j < 5; j++)
+                        if (readlines[i] == "Atoms  # atomic")
                         {
-                            atomdata[i - atomdataline, j] = Convert.ToDouble(wordss[j]);
+                            atomdataline = i + 2;
+                            break;
                         }
                     }
-                    else//原子座標データ前ならbox2に書き込み
+                    //一度抜ける
+
+                    double[,] atomdata = new double[n, 5];//原子座標データを格納する二次元配列
+                    // r0:Atom_ID r1:Atom_Type r2-4:position x-z
+
+                    for (int i = 0; i < readlines.Length; i++)
                     {
-                        textBox2.Text += readlines[i] + Environment.NewLine;
+                        if (atomdataline <= i)//コメント「toms  # atomic」から2行後ろの原子座標データに到達したら
+                        {
+                            string[] wordss = readlines[i].Split(' ');
+                            for (int j = 0; j < 5; j++)
+                            {
+                                atomdata[i - atomdataline, j] = Convert.ToDouble(wordss[j]);
+                            }
+                        }
+                        else//原子座標データ前ならbox2に書き込み
+                        {
+                            textBox2.Text += readlines[i] + Environment.NewLine;
+                        }
+
                     }
 
-                }
 
+                    //ここで配列の計算編集処理
 
-                //ここで配列の計算編集処理
-
-                //座標の最大最小値を表示
-                double[] atomdatax = new double[n];
-                double[] atomdatay = new double[n];
-                double[] atomdataz = new double[n];
-                for (int i = 0; i < n; i++)
-                {
-                    atomdatax[i] = atomdata[i, 2];
-                    atomdatay[i] = atomdata[i, 3];
-                    atomdataz[i] = atomdata[i, 4];
-                }
-                Array.Sort(atomdatax);
-                Array.Sort(atomdatay);
-                Array.Sort(atomdataz);
-
-                label6.Text = Convert.ToString(atomdatax[0]);
-                label7.Text = Convert.ToString(atomdatay[0]);
-                label8.Text = Convert.ToString(atomdataz[0]);
-                label9.Text = Convert.ToString(atomdatax[n-1]);
-                label10.Text = Convert.ToString(atomdatay[n-1]);
-                label11.Text = Convert.ToString(atomdataz[n-1]);
-
-                //その他の計算編集など
-                for (int i = 0; i < n; i++)
-                {
-                    if (atomdata[i,2] > 11.5)
+                    //座標の最大最小値を表示
+                    double[] atomdatax = new double[n];
+                    double[] atomdatay = new double[n];
+                    double[] atomdataz = new double[n];
+                    for (int i = 0; i < n; i++)
                     {
-                        //atomdata[i, 1] += 2;
+                        atomdatax[i] = atomdata[i, 2];
+                        atomdatay[i] = atomdata[i, 3];
+                        atomdataz[i] = atomdata[i, 4];
                     }
-                }
+                    Array.Sort(atomdatax);
+                    Array.Sort(atomdatay);
+                    Array.Sort(atomdataz);
 
-                //progress barの設定
-                progressBar1.Maximum = n;
+                    label6.Text = Convert.ToString(atomdatax[0]);
+                    label7.Text = Convert.ToString(atomdatay[0]);
+                    label8.Text = Convert.ToString(atomdataz[0]);
+                    label9.Text = Convert.ToString(atomdatax[n - 1]);
+                    label10.Text = Convert.ToString(atomdatay[n - 1]);
+                    label11.Text = Convert.ToString(atomdataz[n - 1]);
 
-                //box2に書き込み
-                string boxtext = "";//宣言のみだとnullになる。このようにすることで文字列を追加できる
-                for(int i = 0; i < n; i++)
-                {
-                    for(int j = 0; j < 5; j++)
+                    //その他の計算編集など
+                    for (int i = 0; i < n; i++)
                     {
-                        boxtext += Convert.ToString(atomdata[i,j]) + " ";
+                        if (atomdata[i, 2] > 19.8 && atomdata[i, 1] == 2)
+                        {
+                            atomdata[i, 1] = 6;
+                        }
                     }
-                    boxtext += Environment.NewLine;
-                    progressBar1.PerformStep();
+
+                    //progress barの設定
+                    progressBar1.Maximum = n;
+
+                    //box2に書き込み
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < 5; j++)
+                        {
+                            sb.Append(Convert.ToString(atomdata[i, j]) + " ");
+                        }
+                        sb.Append(Environment.NewLine);
+                        progressBar1.PerformStep();
+                    }
+                    textBox2.Text = sb.ToString();//textboxへの文字列追加はファイル読み込みより時間がかかる。一度に行えば造作もない
                 }
-                textBox2.AppendText(boxtext);//textboxへの文字列追加はファイル読み込みより時間がかかる。一度に行えば造作もない
+            }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////lammpstrj
+            else if(comboBox1.SelectedItem == "lammpstrj") 
+            {
+                int framenumber = 0;
+                int datatype = 8;
+
+                DialogResult dr = openFileDialog1.ShowDialog();
+                if (dr == System.Windows.Forms.DialogResult.OK)
+                {
+                    textBox2.Text = ""; //textboxをリセット
+
+                    filename = openFileDialog1.FileName;
+                    string[] readlines = System.IO.File.ReadAllLines(@filename);//ファイル内容を取得
+
+                    //ITEM: TIMESTEPコメントを探索 → framenumberをカウント & maxatomsを読み取る
+                    int atoms = 0;
+                    int maxatoms = 0;
+                    for (int i = 0; i < readlines.Length; i++)
+                    {
+                        if (readlines[i] == "ITEM: TIMESTEP")
+                        {
+                            framenumber++;
+                            atoms = Convert.ToInt32(readlines[i+3]);
+                            if (atoms > maxatoms) maxatoms = atoms;
+                        }
+                    }
+                    //一度抜ける
+
+                    //ITEM: NUMBER OF ATOMSコメントの行インデックスを配列に格納 & number of atomsをnatoms配列に格納
+                    int[] natomsindex = new int[framenumber];
+                    int[] natoms = new int[framenumber];
+                    int nn = 0;
+                    for (int i = 0; i < readlines.Length; i++)
+                    {
+                        if (readlines[i] == "ITEM: NUMBER OF ATOMS")
+                        {
+                            natomsindex[nn] = i;
+                            natoms[nn] = Convert.ToInt32(readlines[i+1]);
+                            nn++;
+                        }
+                    }
+
+                    double[,,] satomdata = new double[framenumber, maxatoms, datatype];//原子座標データを格納する三次元配列
+                    //l==0 :: r0:timestep r1:number_of_atoms
+                    //l>0  :: r0:Atom_ID r1:Atom_Type r2-4:position x-z r5-7:compute
+
+                    for (int i=0; i<framenumber; i++)
+                    {
+                        for(int j=0; j<natoms[i]; j++)
+                        {
+                            string[] wordss = readlines[natomsindex[i]+7+j].Split(' ');
+                            for (int k = 0; k < datatype; k++)
+                            {
+                                satomdata[i,j,k] = Convert.ToDouble(wordss[k]);
+                            }
+                        }
+                    }
+
+
+                    //その他の計算編集など
+
+
+                    //progress barの設定
+                    progressBar1.Maximum = framenumber;
+
+                    //box2に書き込み
+                    StringBuilder sb = new StringBuilder();
+                    //フレームごとにboxに書き込み
+                    for (int i = 0; i < framenumber; i++)
+                    {
+                        for(int l = natomsindex[i] - 2; l< natomsindex[i] + 7; l++)
+                        {
+                                sb.Append(readlines[l] + Environment.NewLine);
+                        }
+                        for (int j = 0; j < natoms[i]; j++)
+                        {
+                            for (int k = 0; k < datatype; k++)
+                            {
+                                sb.Append(Convert.ToString(satomdata[i, j, k]) + " ");
+                            }
+                            sb.Append(Environment.NewLine);
+                        }
+                        progressBar1.PerformStep();
+                    }
+                    textBox2.Text = sb.ToString();//textboxへの文字列追加はファイル読み込みより時間がかかる。一度に行えば造作もない
+                }
             }
         }
+        
 
 
 
